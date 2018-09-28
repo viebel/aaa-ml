563732da94ed49c13ebca8e32729c35dfd2d791f
Yehonathan Sharvit <viebel@gmail.com>
2018-09-28 17:07:20 +0300

    distribution of typologies

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # big-data
 Big Data
+
+
+
+
+# setup local db
+
+Launch local postgres from docker, from the **root folder**:
+
+~~~bash
+docker-compose up
+~~~
+
+Download a dump from heroku
+
+~~~bash
+heroku pg:backups:download  --app audyx
+~~~
+
+
+Restore the dump in the local db
+
+~~~bash
+pg_restore --verbose --clean --no-acl --no-owner  -h localhost -p 5433 -d audyx --user me latest.dump
+~~~
--- src/big_data/sandbox.clj
+++ src/big_data/sandbox.clj
@@ -7,6 +7,7 @@
 (require '[gadjett.collections :as gadjett :refer [map-object mean select-vals]])
 (require '[gadjett.core :refer [dbg]])
 
+(require '[clojure.pprint :as pprint])
 (require '[clojure.pprint :refer [pprint]])
 (require '[clj-time.core :as t])
 (require '[clj-time.format :as f])
@@ -19,7 +20,7 @@
 ;; Global settings
 
 ;;; db connection
-(def db {:dbtype "postgresql", :dbname "audyx", :host "localhost", :port "5433", :user "me", :password "me"})
+(def db {:dbtype "postgresql", :dbname "audyx_prod", :host "localhost", :port "5433", :user "me", :password "me"})
 
 (map :tablename (j/query db ["select tablename from pg_catalog.pg_tables"]))
 
@@ -200,23 +201,84 @@
 
 (def ttt (all-audiograms))
 
+(count ttt)
+
 (defn audiograms->typologies [audiograms]
   (as-> audiograms $
         (group-by :patient_id $)
         (map-object tests->typology $)))
 
 (defn distribution-of [typologies ear]
-  (->> (map #(get % ear) typologies)
+  (->> (map vals typologies)
+       (apply concat)
        (filter :average-loss)
        frequencies
        (sort-by val)))
 
 (defn typology-distribution [audiograms]
   (let [typologies (vals (audiograms->typologies audiograms))]
-    {"L" (distribution-of typologies "L")
-     "R" (distribution-of typologies "R")}))
+    (distribution-of typologies "L")))
 
-(typology-distribution ttt)
+(def dist (typology-distribution ttt))
+dist
 
+(def aaa '([{:average-loss "normal", :high-loss "mid", :low-loss "light"} 1]
+           [{:average-loss "severe", :high-loss "deep", :low-loss "normal"} 1]
+           [{:average-loss "light", :high-loss "light", :low-loss "severe"} 1]
+           [{:average-loss "light", :high-loss "severe", :low-loss "light"} 1]
+           [{:average-loss "mid", :high-loss "light", :low-loss "severe"} 2]
+           [{:average-loss "light", :high-loss "severe", :low-loss "normal"} 3]
+           [{:average-loss "severe", :high-loss "mid", :low-loss "deep"} 4]
+           [{:average-loss "mid", :high-loss "deep", :low-loss "mid"} 6]
+           [{:average-loss "mid", :high-loss "deep", :low-loss "normal"} 9]
+           [{:average-loss "light", :high-loss "normal", :low-loss "mid"} 15]
+           [{:average-loss "normal", :high-loss "mid", :low-loss "normal"} 15]
+           [{:average-loss "severe", :high-loss "deep", :low-loss "deep"} 35]
+           [{:average-loss "mid", :high-loss "deep", :low-loss "light"} 37]
+           [{:average-loss "light", :high-loss "normal", :low-loss "normal"} 38]
+           [{:average-loss "severe", :high-loss "severe", :low-loss "light"} 50]
+           [{:average-loss "deep", :high-loss "severe", :low-loss "severe"} 57]
+           [{:average-loss "severe", :high-loss "mid", :low-loss "mid"} 60]
+           [{:average-loss "normal", :high-loss "light", :low-loss "light"} 96]
+           [{:average-loss "mid", :high-loss "light", :low-loss "light"} 97]
+           [{:average-loss "mid", :high-loss "severe", :low-loss "severe"} 114]
+           [{:average-loss "severe", :high-loss "deep", :low-loss "light"} 120]
+           [{:average-loss "light", :high-loss "mid", :low-loss "mid"} 153]
+           [{:average-loss "severe", :high-loss "mid", :low-loss "severe"} 191]
+           [{:average-loss "deep", :high-loss "severe", :low-loss "deep"} 197]
+           [{:average-loss "light", :high-loss "normal", :low-loss "light"} 204]
+           [{:average-loss "deep", :high-loss "deep", :low-loss "mid"} 208]
+           [{:average-loss "mid", :high-loss "severe", :low-loss "normal"} 250]
+           [{:average-loss "severe", :high-loss "severe", :low-loss "deep"} 272]
+           [{:average-loss "normal", :high-loss "normal", :low-loss "light"} 319]
+           [{:average-loss "mid", :high-loss "mid", :low-loss "severe"} 347]
+           [{:average-loss "mid", :high-loss "light", :low-loss "mid"} 362]
+           [{:average-loss "light", :high-loss "light", :low-loss "mid"} 529]
+           [{:average-loss "severe", :high-loss "deep", :low-loss "mid"} 1226]
+           [{:average-loss "severe", :high-loss "deep", :low-loss "severe"} 1237]
+           [{:average-loss "mid", :high-loss "mid", :low-loss "normal"} 1629]
+           [{:average-loss "normal", :high-loss "light", :low-loss "normal"} 1765]
+           [{:average-loss "deep", :high-loss "deep", :low-loss "severe"} 2548]
+           [{:average-loss "mid", :high-loss "severe", :low-loss "light"} 3808]
+           [{:average-loss "light", :high-loss "light", :low-loss "normal"} 4351]
+           [{:average-loss "normal", :high-loss "normal", :low-loss "normal"} 4945]
+           [{:average-loss "severe", :high-loss "severe", :low-loss "mid"} 5189]
+           [{:average-loss "severe", :high-loss "severe", :low-loss "severe"} 5826]
+           [{:average-loss "light", :high-loss "light", :low-loss "light"} 6068]
+           [{:average-loss "light", :high-loss "mid", :low-loss "normal"} 6430]
+           [{:average-loss "mid", :high-loss "severe", :low-loss "mid"} 8700]
+           [{:average-loss "light", :high-loss "mid", :low-loss "light"} 9482]
+           [{:average-loss "deep", :high-loss "deep", :low-loss "deep"} 19346]
+           [{:average-loss "mid", :high-loss "mid", :low-loss "mid"} 24498]
+           [{:average-loss "mid", :high-loss "mid", :low-loss "light"} 28636]))
 
+(pprint/print-table (keys aaa))
+(select-vals [:average-loss :high-loss :low-loss] (get aaa "L"))
 
+(defn dist->table [dist]
+  (with-out-str (let [total (dbg (apply + (map second dist)))]
+                  (pprint/print-table (reverse (map (fn [[k v]] (assoc k :count v :ratio (format "%.2f" (* 100 (float (/ v total)))))) dist))))))
+
+(print (dist->table dist))
+
+(format "%.2f" 1.2392423423)
